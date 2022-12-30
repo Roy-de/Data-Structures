1ae39d81ece83bd2afa4a95fe844d5d90a68f0c7
Roy-de <72699015+Roy-de@users.noreply.github.com>
2022-12-30 22:20:48 +0300

    Test

---
+++ LinkedList.c
@@ -0,0 +1,20 @@
+//Singly linked list in C
+#include <stdio.h>;
+#include <stdlib.h>;
+
+//Node creation
+struct DataNode
+{
+    /* data */
+    char Data[50];
+    struct DataNode *next;
+};
+struct DataNode *head, *ptr;
+
+ptr = (struct DataNode*)malloc(sizeof(struct DataNode*));
+
+void main(){
+    printf("************MAIN MENU**********\n");
+    printf("Choose an option\n");
+    printf("")
+}
